2be571bd26020dbfc38ff5586981e7526ff84c02
fahrurozi <m.fahruu.rozii@gmail.com>
2025-01-08 21:44:58 +0700

    feat: add prometheus and grafana

---
+++ api/v1/middleware/log_middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Request received: ", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
