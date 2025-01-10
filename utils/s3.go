86d255e96ee99f7b83a48d67351e357d6e3abefc
Muhammad Fahru Rozi <m.fahruu.rozii@gmail.com>
2025-01-10 22:25:45 +0700

    feat: fixing config aws

--- utils/s3.go
+++ utils/s3.go
@@ -13,21 +13,32 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+var (
+	awsRegion     string
+	awsAccessKey  string
+	awsSecretKey  string
+	awsBucketName string
+)
+
+func init() {
+	awsRegion = os.Getenv("AWS_REGION")
+	awsAccessKey = os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	awsBucketName = os.Getenv("AWS_S3_BUCKET_NAME")
+}
+
 // CreateSession creates a new AWS session
 func CreateSession() (*session.Session, error) {
-	region := os.Getenv("AWS_REGION")
-	if region == "" {
-		region = "us-east-1"
+	if awsRegion == "" {
+		awsRegion = "us-east-1"
 	}
 
-	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
-	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-
 	return session.NewSession(&aws.Config{
-		Region: aws.String(region),
+		Region: aws.String(awsRegion),
 		Credentials: credentials.NewStaticCredentials(
-			accessKey,
-			secretKey,
+			awsAccessKey,
+			awsSecretKey,
 			"",
 		),
 	})
@@ -35,16 +46,15 @@ func CreateSession() (*session.Session, error) {
 
 // UploadFileToS3 uploads a file to AWS S3 and returns the file URL
 func UploadFileToS3(file multipart.File, filename string) (string, error) {
-	bucketName := os.Getenv("AWS_BUCKET_NAME")
-	region := os.Getenv("AWS_REGION")
-
 	log.Println("AWS_ACCESS_KEY_ID:", os.Getenv("AWS_ACCESS_KEY_ID"))
 	log.Println("AWS_SECRET_ACCESS_KEY:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
+
+	log.Println(awsRegion + "|" + awsAccessKey + "|" + awsSecretKey + "|" + awsBucketName + "|")
 	sess, err := CreateSession()
 	if err != nil {
 		return "Error create session: ", err
 	}
-	if region == "" {
+	if awsRegion == "" {
 		log.Fatal("Error: AWS_REGION environment variable is not set")
 	}
 
@@ -53,7 +63,7 @@ func UploadFileToS3(file multipart.File, filename string) (string, error) {
 	key := fmt.Sprintf("uploads/%d_%s", time.Now().Unix(), filename)
 
 	_, err = svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(bucketName),
+		Bucket: aws.String(awsBucketName),
 		Key:    aws.String(key),
 		Body:   file,
 	})
@@ -63,5 +73,5 @@ func UploadFileToS3(file multipart.File, filename string) (string, error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key), nil
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", awsBucketName, awsRegion, key), nil
 }
